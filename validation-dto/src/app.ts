826e710103bcccda5f0411b22398e033c961120f
junior rosa <junior_duff_sm@hotmail.com>
2022-12-20 10:10:09 -0300

    feat: remove unused code

--- validation-dto/src/app.ts
+++ validation-dto/src/app.ts
@@ -2,12 +2,9 @@ import "reflect-metadata";
 
 import bodyParser from "body-parser";
 import { Container } from "inversify";
-import * as prettyjson from "prettyjson";
 import { getRouteInfo, InversifyExpressServer } from "inversify-express-utils";
 
 import "./validation.controller";
-import {NextFunction , Request, Response} from "express";
-import {BodyValidation} from "./errors/body-validation";
 
 let container = new Container();
 let server = new InversifyExpressServer(container);
@@ -19,24 +16,8 @@ server.setConfig((app) => {
     })
   );
   app.use(bodyParser.json());
-
-    app.use(
-        (err: Error, request: Request, response: Response, next: NextFunction) => {
-            if (err instanceof BodyValidation) {
-                console.log(err)
-                return response.status(400).json({ message: err.message });
-            }
-            return response.status(500).json({
-                status: "error",
-                message: `Internal server error - ${err.message}`,
-            });
-        }
-    );
 });
 
 let app = server.build();
-
-
 const routeInfo = getRouteInfo(container);
-console.log(prettyjson.render(routeInfo));
 app.listen(3000);
